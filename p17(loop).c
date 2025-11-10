e2671c09a0876a657d43955783e7a1d1e65ed83a
divysolanki2725-rgb <divysolanki2725@gmail.com>
2025-11-10 23:08:33 +0530

    Implement value counting for positive, negative, zero
    This program counts the number of positive, negative, and zero values from user input.

---
+++ p17(loop).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, num;
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("Enter 200 values:\n");
+
+    for (i = 0; i < 200; i++) {
+        scanf("%d", &num);
+
+        if (num > 0)
+            positive++;
+        else if (num < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Positive numbers = %d\n", positive);
+    printf("Negative numbers = %d\n", negative);
+    printf("Zeroes = %d\n", zero);
+
+    return 0;
+}
