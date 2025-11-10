adfb85c67430dd8aa98ea2a42f0edc797f76161f
divysolanki2725-rgb <divysolanki2725@gmail.com>
2025-11-10 23:27:10 +0530

    Add Fibonacci sequence generator in p34(loop).c
    Implement Fibonacci sequence calculation.

---
+++ p34(loop).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 1, b = 1, c;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    if (n >= 1) printf("%d\n", a);
+    if (n >= 2) printf("%d\n", b);
+
+    for (i = 3; i <= n; i++) {
+        c = a + b;
+        printf("%d\n", c);
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
