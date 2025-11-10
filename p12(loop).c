663294de168db4a20a4bfaad935b102f0058480d
divysolanki2725-rgb <divysolanki2725@gmail.com>
2025-11-10 23:01:38 +0530

    Create p12(loop).c

---
+++ p12(loop).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        printf("Divy Solanki\n");   // Replace with your name if needed
+    }
+
+    return 0;
+}
